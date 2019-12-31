9bb6f015462710266c77a1270d4fe0a06190a8b1
vishalibitwar <vish31ibitwar@gmail.com>
2019-12-31 20:30:43 +0530

    Animation Added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
-
+const h1 =  document.getElementById('h1');
 document.querySelector('#changeBtn').addEventListener('click', function(e)
 { randomQuote();
 });
 
 async function randomQuote() {
   const response = await fetch('https://api.quotable.io/random');
-  const data = await response.json();
-  document.getElementById('h1').innerText = data.content;
+  const data = await response.json(); 
+  animate(h1, 'flipInY');
+  h1.innerText = data.content;
 }
 
+
+function animate(element, animation) {
+  element.classList.add(animation);
+  const wait = setTimeout(() => {
+    element.classList.remove('animated', animation);
+  }, 1000);
+}
