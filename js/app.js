4f522227f71c7d672bcfd65279d363384e603f21
vishalibitwar <vish31ibitwar@gmail.com>
2019-12-31 21:32:07 +0530

    :disappointed:

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ changeBtn.addEventListener('click', function (e) {
   fetch('https://api.quotable.io/random')
     .then(response => response.json())
     .then(data => {
-      animate(h1, 'flipInY');
+      // animate(h1, 'flipInY');
       h1.innerText = data.content;
     });
       e.preventDefault();
@@ -14,6 +14,6 @@ changeBtn.addEventListener('click', function (e) {
 function animate(element, animation) {
   element.classList.add(animation);
   const wait = setTimeout(() => {
-    element.classList.remove('animated', animation);
+    element.classList.remove(animation);
   }, 1000);
 }
