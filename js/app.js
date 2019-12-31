37d3d9a012cf942951de523a0a5db342438f54cf
vishalibitwar <vish31ibitwar@gmail.com>
2019-12-31 20:46:34 +0530

    fetch instead of async

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-const h1 =  document.getElementById('h1');
-document.querySelector('#changeBtn').addEventListener('click', function(e)
-{ randomQuote();
+const h1 = document.getElementById('h1');
+document.querySelector('#changeBtn').addEventListener('click', function () {
+  // randomQuote();
+  fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      animate(h1, 'flipInY');
+      h1.innerText = data.content;
+    })
 });
 
-async function randomQuote() {
-  const response = await fetch('https://api.quotable.io/random');
-  const data = await response.json(); 
-  animate(h1, 'flipInY');
-  h1.innerText = data.content;
-}
+// async function randomQuote() {
+//   const response = await fetch('https://api.quotable.io/random');
+//   const data = await response.json();
+//   animate(h1, 'flipInY');
+//   h1.innerText = data.content;
+// }
 
 
 function animate(element, animation) {
