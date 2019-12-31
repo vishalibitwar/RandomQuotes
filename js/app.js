b4daa3e3e8906556247ce301947bd1371084e564
vishalibitwar <vish31ibitwar@gmail.com>
2019-12-31 21:15:42 +0530

    mobile click :tired_face:

--- js/app.js
+++ js/app.js
@@ -2,23 +2,14 @@ const h1 = document.getElementById('h1');
 const changeBtn = document.getElementById('changeBtn');
 
 changeBtn.addEventListener('click', function (e) {
-  // randomQuote();
   fetch('https://api.quotable.io/random')
     .then(response => response.json())
     .then(data => {
       animate(h1, 'flipInY');
       h1.innerText = data.content;
     });
-  e.preventDefault();
+      e.preventDefault();
 });
-
-// async function randomQuote() {
-//   const response = await fetch('https://api.quotable.io/random');
-//   const data = await response.json();
-//   animate(h1, 'flipInY');
-//   h1.innerText = data.content;
-// }
-
 
 function animate(element, animation) {
   element.classList.add(animation);
