aa38c7a4f0b43a8fbc48f65841d380a5cbc4b4fd
vishalibitwar <vish31ibitwar@gmail.com>
2019-12-31 22:00:05 +0530

    touchstart added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 const h1 = document.getElementById('h1');
 const changeBtn = document.getElementById('changeBtn');
 
-changeBtn.addEventListener('click', function (e) {
+
+changeBtn.addEventListener('touchstart', showQuote);
+changeBtn.addEventListener('click', showQuote);
+
+function showQuote() {
   fetch('https://api.quotable.io/random')
     .then(response => response.json())
     .then(data => {
       animate(h1, 'flipInY');
       h1.innerText = data.content;
     });
-  e.preventDefault();
-});
+}
+
 
 function animate(element, animation) {
   element.classList.add(animation);
